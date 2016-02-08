ad8859c60d5036aa9b8c7995c110644a87749fb9
Sam Bender <rednebmas@gmail.com>
2016-02-07 23:27:12 -0800

    Initial Commit

---
+++ MotorolaCodingChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MotorolaCodingChallenge
+//
+//  Created by Sam Bender on 2/7/16.
+//  Copyright © 2016 Sam Bender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
